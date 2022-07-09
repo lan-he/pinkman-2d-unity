773bb1e88c9ae46a7364d5e2bcb597a2d55c8f1c
何明暄 <hemingxuan@hemingxuans-MacBook-Pro.local>
2022-07-09 21:11:37 +0800

    打包

--- Assets/Scripts/Player/PlayerCollect.cs
+++ Assets/Scripts/Player/PlayerCollect.cs
@@ -1,41 +1,72 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 // 触发器判断 收集物品
 public class PlayerCollect : MonoBehaviour
 {
     public int apple; // 收集apple数量
+
     public Text appleText; // 收集apple数量显示
+
     public GameObject panel;
+
     public Text panelText;
+
+    private Animator anim; // 动画控制器
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>(); // 动画控制器赋值
+    }
 
     private void OnTriggerEnter2D(Collider2D collision) // 收集物品 人物碰撞到2d标签时候的回调
     {
-        if (collision.tag == "Collection")
+        if (
+            collision.tag == "Collection" // 碰到可收集物品
+        )
         {
-            Collectable collectable = collision.gameObject.GetComponent<Collectable>();
+            Collectable collectable =
+                collision.gameObject.GetComponent<Collectable>();
             collectable.collectYes();
             apple += 1;
             appleText.text = apple.ToString() + "/5";
         }
-        if (collision.tag == "NextLevel")
+        if (
+            collision.tag == "NextLevel" // 碰到下一关
+        )
         {
             if (apple >= 5)
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                SceneManager
+                    .LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
             else
             {
                 panelText.text = "还需收集" + (5 - apple) + "个苹果";
                 panel.SetActive(true);
             }
-
         }
-        if (collision.tag == "DieLine")
+        if (
+            collision.tag == "DieLine" // 碰到死亡线
+        )
         {
             Invoke("Restart", 1f);
         }
+        if (
+            collision.tag == "Prickle" // 碰到陷阱
+        )
+        {
+            SwitchAnim();
+            Invoke("Restart", 1f);
+        }
     }
+
+    public void SwitchAnim() // 控制动画
+    {
+        anim.SetInteger("state", 5); // 转换成成整数类型并赋值动画变量
+    }
+
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.tag == "NextLevel")
@@ -43,6 +74,7 @@ public class PlayerCollect : MonoBehaviour
             panel.SetActive(false);
         }
     }
+
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,36 +3,58 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+
 // 角色控制
 public class PlayerController : MonoBehaviour
 {
     //[SerializeField] private Rigidbody2D rb; // 缸体 private私有变量
     private Rigidbody2D rb; // 缸体
+
     private Animator anim; // 动画控制器
+
     private Collider2D coll; // 碰撞体
+
     public ParticleSystem playerPs; // 粒子系统
+
     // 移动=========
     public int speed; // 声明速度
+
     private float moveX;
+
     private bool facingRight = true;
+
     // 跳跃========
     [Range(1, 10)]
     public int jumpForce; // 跳跃系数
+
     private bool moveJump; // 跳跃输
+
     private bool jumpHold; // 长按跳跃
+
     public bool isUpGround; // 上一帧是否在地面上
+
     public bool isGround; // 是否在地面上
+
     public int jumpCount = 2; // 跳跃次数
+
     private bool isJump; // 传递作用
+
     public Transform groundCheck; //地面监测点
+
     public LayerMask ground; // 声明碰撞体图层
+
     [SerializeField]
     private Vector2 boxSize;
+
     private float fallAddition = 3f; // 下落重力加成
+
     private float jumpAddition = 2f; // 跳跃重力加成
+
     public AudioSource jumpAudio;
+
     // 受伤=======
     private bool isHurt; // 是否受伤
+
     // 动画=======
     private enum playerState
     {
@@ -40,14 +62,17 @@ public class PlayerController : MonoBehaviour
         run,
         jump,
         fall,
-        hurt
-    } // 枚举 {静止,跑动,起跳,降落}
+        hurt,
+        die
+    } // 枚举 {静止,跑动,起跳,降落，受伤，死亡}
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>(); //缸体赋值
         anim = GetComponent<Animator>(); // 动画控制器赋值
         coll = GetComponent<Collider2D>();
     }
+
     // Update 每一帧都回执行
     private void Update()
     {
@@ -66,11 +91,13 @@ public class PlayerController : MonoBehaviour
         }
         SwitchAnim();
     }
+
     private void FixedUpdate() // FixedUpdate固定时间执行
     {
         isUpGround = isGround;
         CheckGround();
     }
+
     void Movement() // 角色移动
     {
         rb.velocity = new Vector2(moveX * speed, rb.velocity.y); // 横向施加一个力
@@ -84,6 +111,7 @@ public class PlayerController : MonoBehaviour
             Filp();
         }
     }
+
     private void Filp() // 角色反转
     {
         facingRight = !facingRight;
@@ -92,18 +120,21 @@ public class PlayerController : MonoBehaviour
         transform.localScale = playerScale;
         ParticleSystem();
     }
+
     private void CheckGround()
     {
         // isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, ground); // 判断是否接触地面 圆形第二个参数是圆的半径
         isGround =
             Physics2D.OverlapBox(groundCheck.position, boxSize, 0, ground); // 判断是否接触地面 方形第二个参数是v2,第三个参数是角度
     }
+
     private void OnDrawGizmos()
     {
         // Gizmos.DrawWireSphere(groundCheck.position, 0.1f); // 可视化圆形物体
         Gizmos.DrawWireCube(groundCheck.position, boxSize); // 可视化盒子
         Gizmos.color = Color.red;
     }
+
     private void Jump() // 角色跳跃
     {
         if (!isUpGround && isGround)
@@ -114,6 +145,7 @@ public class PlayerController : MonoBehaviour
         {
             jumpAudio.Play();
             rb.velocity = Vector2.up * jumpForce;
+
             // rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); // 施加一个瞬间的力
             jumpCount--;
             isJump = false;
@@ -131,8 +163,8 @@ public class PlayerController : MonoBehaviour
         {
             rb.gravityScale = 1f; // 重置缸体重力
         }
-        
     }
+
     void SwitchAnim() // 控制动画
     {
         playerState states; // 由枚举对象赋值
@@ -156,12 +188,14 @@ public class PlayerController : MonoBehaviour
         {
             states = playerState.hurt;
         }
-        anim.SetInteger("state", (int)states); // 转换成成整数类型并赋值动画变量
+        anim.SetInteger("state", (int) states); // 转换成成整数类型并赋值动画变量
     }
+
     private void ParticleSystem() // 粒子系统
     {
         playerPs.Play();
     }
+
     private void OnCollisionEnter2D(Collision2D collision) // 消灭敌人 缸体之间触碰的回调
     {
         if (
@@ -173,6 +207,7 @@ public class PlayerController : MonoBehaviour
             if (!isGround && rb.velocity.y < -0.1f)
             {
                 mushroom.HitAnimator();
+
                 // Destroy(collision.gameObject);
                 rb.velocity = new Vector2(rb.velocity.x, 6);
             }
@@ -192,11 +227,13 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
+
     public void HurtDown() // 受伤结束 由动画事件触发
     {
         rb.velocity = new Vector2(0, 0);
         isHurt = false;
     }
+
     void KillPlayer()
     {
         // Invoke("KillPlayer",dieTime); // 延迟事件
